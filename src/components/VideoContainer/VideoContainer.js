65abf6402669038e439d7b697fd45d3782542827
Volodymyr <kartaiev@gmail.com>
2020-03-06 22:31:35 +0200

    video-slider redone

--- src/components/VideoContainer/VideoContainer.js
+++ src/components/VideoContainer/VideoContainer.js
@@ -12,21 +12,14 @@ const swipePower = (offset, velocity) => {
 const variants = {
   center: {
     zIndex: 1,
-    x: 0,
     opacity: 1,
   },
-  enter: direction => {
-    return {
-      x: direction > 0 ? 1000 : -1000,
-      opacity: 0,
-    };
+  enter: {
+    opacity: 0,
   },
-  exit: direction => {
-    return {
-      zIndex: 0,
-      x: direction < 0 ? 1000 : -1000,
-      opacity: 0,
-    };
+  exit: {
+    zIndex: 0,
+    opacity: 0,
   },
 };
 
@@ -53,10 +46,7 @@ const VideoContainer = () => {
           initial="enter"
           animate="center"
           exit="exit"
-          transition={{
-            x: { type: 'spring', stiffness: 300, damping: 200 },
-            opacity: { duration: 0.2 },
-          }}
+          transition={{ type: 'tween', ease: 'easeIn', duration: 1 }}
           drag="x"
           dragConstraints={{ left: 0, right: 0 }}
           dragElastic={1}
