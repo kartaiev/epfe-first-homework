e062823974925f72a86514cb8a7144c6eaaa93e6
Denys Stetsenko2 <denys_stetsenko2@epam.com>
2020-03-06 19:33:00 +0200

    Remove unneded fontfamily declarations, add global styling to Copyright

--- src/components/Copyright/Copyright.js
+++ src/components/Copyright/Copyright.js
@@ -3,9 +3,15 @@ import './Copyright.scss';
 
 const Copyright = () => (
   <div className="Copyright">
-    <span className="Copyright__name">Made by <span>MDVID</span></span> 
+    <span className="Copyright__author">
+      Made by <span className="Copyright__name">MDVID</span>
+    </span>
     <picture>
-      <img className="Copyright__logo" src={require('./images/EP_AM-logo.png')} alt="logo"/>
+      <img
+        className="Copyright__logo"
+        src={require('./images/EP_AM-logo.png')}
+        alt="logo"
+      />
     </picture>
   </div>
 );
