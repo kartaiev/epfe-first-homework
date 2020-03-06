c4b449873093640130ca2940b0788b50c199aeaf
Denys Stetsenko2 <denys_stetsenko2@epam.com>
2020-03-06 19:48:21 +0200

    Improve Central component, remove unneded wrapper for SideMenu

--- src/components/Central/Central.js
+++ src/components/Central/Central.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Central.scss';
 import SideMenuContainer from '../SideMenuContainer/SideMenuContainer';
+import SliderAssets from '../SliderAssets/SliderAssets';
 
 const Central = () => {
   return (
     <div className="Central">
       <SideMenuContainer />
+      <SliderAssets />
+      <div className="Central__invisible-block">
+        <SideMenuContainer />
+      </div>
     </div>
   );
 };
--- src/components/SideMenuContainer/SideMenuContainer.js
+++ src/components/SideMenuContainer/SideMenuContainer.js
@@ -5,12 +5,10 @@ import './SideMenuContainer.scss';
 const SideMenuContainer = () => {
   return (
     <div className="SideMenuContainer">
-      <div className="SideMenuContainer__wrapper">
-        <SideMenuElement name="Home" link="#" />
-        <SideMenuElement name="About" link="#" />
-        <SideMenuElement name="Team" link="#" />
-        <SideMenuElement name="Contact" link="#" />
-      </div>
+      <SideMenuElement name="Home" link="#" />
+      <SideMenuElement name="About" link="#" />
+      <SideMenuElement name="Team" link="#" />
+      <SideMenuElement name="Contact" link="#" />
     </div>
   );
 };
