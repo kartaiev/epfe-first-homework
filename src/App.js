7e7c9955bbd4433e9489f9051fafb46cd4c02f55
Volodymyr <kartaiev@gmail.com>
2020-03-06 13:52:56 +0200

    draft version of the video-slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Home from './Home';
 
 const App = () => (
-  <div>
-    <h1>EP/AM</h1>
-  </div>
+  <>
+    <Home />
+  </>
 );
 
 export default App;
