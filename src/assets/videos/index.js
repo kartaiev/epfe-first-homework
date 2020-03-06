f7833f69038115b98db0c283a6df3d333e5ebd1e
Volodymyr <kartaiev@gmail.com>
2020-03-07 00:45:36 +0200

    new videos added

--- src/assets/videos/index.js
+++ src/assets/videos/index.js
@@ -1,6 +1,6 @@
-import video1 from './Thunderstruck.mp4';
-import video2 from './HighwaytoHell.mp4';
-import video3 from './T.N.T.mp4';
-import video4 from './BlackinBlack.mp4';
+import video1 from './acdc2-loop.webm';
+import video2 from './acdc1-loop.webm';
+import video3 from './acdc3-loop.webm';
+import video4 from './acdc4-loop.webm';
 
 export default [video1, video2, video3, video4];
