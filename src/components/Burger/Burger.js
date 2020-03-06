33f542f28f9618ee7c860972628aad4ec8d1817a
Volodymyr <kartaiev@gmail.com>
2020-03-07 00:23:50 +0200

    hamburger?

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,13 +1,49 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { AnimatePresence, motion, useCycle } from 'framer-motion';
 import './burger.scss';
 
 const Burger = () => {
+  const [on, setOn] = useCycle(false, true);
+
   return (
-    <div className="TourMap__Burger Burger">
-      <div className="Burger__controller" />
-      <div className="Burger__controller" />
-    </div>
+    <motion.div onTap={setOn} className="TourMap__Burger Burger">
+      <motion.div
+        animate={
+          on
+            ? {
+                rotate: 45,
+                x: '-1px',
+                y: '5px',
+                backgroundColor: 'rgba(252, 72, 24, 1)',
+              }
+            : {
+                rotate: 0,
+                x: 0,
+                y: 0,
+                backgroundColor: 'rgba(240, 240, 240, 1)',
+              }
+        }
+        className="Burger__controller"
+      />
+      <motion.div
+        animate={
+          on
+            ? {
+                rotate: -45,
+                x: '-1px',
+                y: '-5.5px',
+                backgroundColor: 'rgba(252, 72, 24, 1)',
+              }
+            : {
+                rotate: 0,
+                x: 0,
+                y: 0,
+                backgroundColor: 'rgba(240, 240, 240, 1)',
+              }
+        }
+        className="Burger__controller"
+      />
+    </motion.div>
   );
 };
 export default Burger;
