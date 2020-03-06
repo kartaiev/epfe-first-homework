41b8499beac40b9fd3645473f6d579dda77c95a5
Denys Stetsenko <denys_stetsenko2@epam.com>
2020-03-06 12:46:17 +0200

    Create basic App environment for future use

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Footer.scss';
+
+// // Prototypal Class style
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is a container</h1>
+      </div>
+    );
+  }
+}
+
+export default Footer;
