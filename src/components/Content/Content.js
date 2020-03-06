f935b663c0e434813e083e6ac43921ca889ea6cd
Maksym Koval1 <Maksym_Koval1@epam.com>
2020-03-06 18:52:56 +0200

    Added slider assets layout

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Footer from '../Footer/Footer';
 import TourMap from '../TourMap/TourMap';
+import SliderAssets from '../SliderAssets/SliderAssets';
 
 // // Prototypal Class style
 const Content = () => {
   return (
     <div>
       <TourMap />
+      <SliderAssets />
       <Footer />
     </div>
   );
---
+++ src/components/SliderAssets/SliderAssets.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './SliderAssets.scss';
+
+export default function SliderAssets() {
+  return (
+    <div className="slider__assets">
+      <div className="slider__trackname">Highway to Front</div>
+      <button type="button" className="slider__button button">
+        Play
+      </button>
+    </div>
+  );
+}
