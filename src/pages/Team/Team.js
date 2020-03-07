54a1ecfb3e0ec090715cca485f365826280c78a2
Iryna Kozlovska <ryba4ok@gmail.com>
2020-03-07 18:29:09 +0200

    feat: create Logo component, add team page

--- src/pages/Team/Team.js
+++ src/pages/Team/Team.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import './Team.scss';
+import SideMenuContainer from '../../components/SideMenuContainer/SideMenuContainer';
+import Logo from '../../components/Logo/Logo';
 
 const Team = () => {
   return (
-    <div>
-      <h1>Team page</h1>
+    <div className="block">
+      <aside className="aside">
+        <Logo />
+        <SideMenuContainer />
+      </aside>
+      <div className="wrapper">
+        <nav className="team">
+          <ul className="team__list">
+            <li className="team__list-item team__list-item--first">
+              <a className="team__list-link" href="#">
+                <span className="team__member team__member--guitar">Max</span>
+              </a>
+            </li>
+            <li className="team__list-item team__list-item--second">
+              <a className="team__list-link" href="#">
+                <span className="team__member team__member--drumstick">
+                  Dima
+                </span>
+              </a>
+            </li>
+            <li className="team__list-item team__list-item--third">
+              <a className="team__list-link" href="#">
+                <span className="team__member team__member--sing">Vlad</span>
+              </a>
+            </li>
+            <li className="team__list-item team__list-item--fourth">
+              <a className="team__list-link" href="#">
+                <span className="team__member team__member--drumstick">
+                  Iryna
+                </span>
+              </a>
+            </li>
+            <li className="team__list-item team__list-item--fifth">
+              <a className="team__list-link" href="#">
+                <span className="team__member team__member--guitar">Dan</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 };
