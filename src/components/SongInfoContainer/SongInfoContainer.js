64854c06530446e792195e6dac351b2242597f7a
unknown <d.kashmensky@gmail.com>
2020-03-08 21:27:37 +0200

    Add song number component, include it in song info container

--- src/components/SongInfoContainer/SongInfoContainer.js
+++ src/components/SongInfoContainer/SongInfoContainer.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SongInfoElement from '../SongInfoElement/SongInfoElement';
+import SongNumber from '../SongNumber/SongNumber';
 import './SongInfoContainer.scss';
 
 const SongInfoContainer = props => {
-  const { album, name } = props;
+  const { album, name, currentNumber, totalNumber } = props;
 
   return (
     <div className="SongInfoContainer">
       <SongInfoElement name={album} title="Album" />
       <SongInfoElement name={name} title="Track" />
+      <SongNumber current={currentNumber} total={totalNumber} />
     </div>
   );
 };
@@ -17,11 +19,15 @@ const SongInfoContainer = props => {
 SongInfoContainer.propTypes = {
   album: PropTypes.string,
   name: PropTypes.string,
+  currentNumber: PropTypes.number,
+  totalNumber: PropTypes.number,
 };
 
 SongInfoContainer.defaultProps = {
   album: 'Girls got rhythm',
   name: 'Highway to hell',
+  currentNumber: 1,
+  totalNumber: 4,
 };
 
 export default SongInfoContainer;
