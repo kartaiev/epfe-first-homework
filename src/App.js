b9a643eb6366afb1e803afd02be29e976c2bb55e
Volodymyr <kartaiev@gmail.com>
2020-03-08 18:17:43 +0200

    slider done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.scss';
 import { Route, Switch } from 'react-router-dom';
-import Home from './Pages/Home/Home';
-import About from './Pages/About/About';
-import Team from './Pages/Team/Team';
-import Contacts from './Pages/Contacts/Contacts';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Team from './pages/Team/Team';
+import Contacts from './pages/Contacts/Contacts';
+import { SliderProvider } from './contexts/SliderContext';
 
 const App = () => {
   return (
     <>
       <Switch>
-        <Route path="/" component={Home} exact />
+        <SliderProvider>
+          <Route path="/" component={Home} exact />
+        </SliderProvider>
         <Route path="/about" component={About} />
         <Route path="/team" component={Team} />
         <Route path="/contacts" component={Contacts} />
