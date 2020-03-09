6410e9c1a5fba418264ad33ecb69dae8a42f471e
Denys Stetsenko2 <denys_stetsenko2@epam.com>
2020-03-09 12:19:39 +0200

    Fix SideMenu and central SliderAssets components styling

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from '../Header/Header';
 import Central from '../Central/Central';
 import Footer from '../Footer/Footer';
+import SideMenuContainer from '../SideMenuContainer/SideMenuContainer';
 
 import './Content.scss';
 
 const Content = () => {
   return (
     <div className="Content">
+      <SideMenuContainer />
       <Header />
       <Central />
       <Footer />
