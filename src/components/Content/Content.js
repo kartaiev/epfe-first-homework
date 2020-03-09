fff1be1c31a31512297b4258c09f85f3e4d98487
max <maxonchil@gmail.com>
2020-03-09 16:55:52 +0200

    *Finish functionality of tourMap page
    *Added animation of tourmap
    *Little fix

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -3,16 +3,14 @@ import Header from '../Header/Header';
 import Central from '../Central/Central';
 import Footer from '../Footer/Footer';
 import TourMap from '../TourMap/TourMap';
-import TourMapController from '../TourMapController/TourMapController';
 
 import './Content.scss';
 
 const Content = ({ state, toogle }) => {
   return (
     <div className="Content">
-      <Header state={state} toogle={toogle}/>
+      <Header state={state} toogle={toogle} />
       <TourMap state={state} toogle={toogle} />
-
       <Central />
       <Footer />
     </div>
