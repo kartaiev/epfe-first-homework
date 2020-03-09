048b7885b87a7702603b9bbc265b02f5db8daa5d
Denys Stetsenko2 <denys_stetsenko2@epam.com>
2020-03-09 16:53:44 +0200

    Create base MemberPage structure

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React, { useEffect } from 'react';
 import './App.scss';
 import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home/Home';
-import About from './pages/About/About';
 import Team from './pages/Team/Team';
-import Contacts from './pages/Contacts/Contacts';
+import Member from './pages/Member/Member';
 import { SliderProvider } from './contexts/SliderContext';
 
 const App = () => {
@@ -36,9 +35,8 @@ const App = () => {
   return (
     <>
       <Switch>
-        <Route path="/about" component={About} />
         <Route path="/team" component={Team} />
-        <Route path="/contacts" component={Contacts} />
+        <Route path="/member" component={Member} />
         <SliderProvider>
           <Route path="/" component={Home} exact />
         </SliderProvider>
