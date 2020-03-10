94180a8a91948caccefeea8a80d838c9b70922ee
Volodymyr <kartaiev@gmail.com>
2020-03-10 21:51:46 +0200

    functionality done

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import './burger.scss';
 
-const Burger = ({ state, toogle }) => {
+const Burger = ({ state, toggle }) => {
   return state ? (
-    <div className="TourMap__Burger Burger" onClick={toogle}>
+    <div className="TourMap__Burger Burger" onClick={toggle}>
       <div className="Burger__controller is-open" />
       <div className="Burger__controller is-open" />
     </div>
   ) : (
-    <div className="TourMap__Burger Burger" onClick={toogle}>
+    <div className="TourMap__Burger Burger" onClick={toggle}>
       <div className="Burger__controller is-close" />
       <div className="Burger__controller is-close" />
     </div>
--- src/components/TourMapController/TourMapController.js
+++ src/components/TourMapController/TourMapController.js
@@ -4,22 +4,22 @@ import './TourMapController.scss';
 
 import Burger from '../Burger/Burger';
 
-const TourMapController = ({ state, toogle }) => {
+const TourMapController = ({ state, toggle }) => {
   return (
     <div className="TourMapController">
       <div className="TourMapController__inner">
         {state && (
-          <span className="TourMapController__text is-checked" onClick={toogle}>
+          <span className="TourMapController__text is-checked" onClick={toggle}>
             Close
           </span>
         )}
         {!state && (
-          <span className="TourMapController__text is-checked" onClick={toogle}>
+          <span className="TourMapController__text is-checked" onClick={toggle}>
             Tourmap
           </span>
         )}
 
-        <Burger state={state} toogle={toogle} />
+        <Burger state={state} toggle={toggle} />
       </div>
     </div>
   );
