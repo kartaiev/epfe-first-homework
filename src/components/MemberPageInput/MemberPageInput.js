ea092069e73dd0bdd960571173e39237f7c3e6ba
max <maxonchil@gmail.com>
2020-03-10 23:17:58 +0200

    start to work about personal feature

---
+++ src/components/MemberPageInput/MemberPageInput.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './MemberPageInput.scss';
+
+export default function MemberPageInput() {
+  return <input id="themeChange" className="MemberPageInput" type="checkbox" />;
+}
--- src/pages/Member/Member.js
+++ src/pages/Member/Member.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import SideMenuContainer from '../../components/SideMenuContainer/SideMenuContainer';
 import Copyright from '../../components/Copyright/Copyright';
 import './Member.scss';
+import MemberPageInput from '../../components/MemberPageInput/MemberPageInput';
 
 import team from '../../data/team.json';
 
@@ -17,22 +18,26 @@ const Member = props => {
   const member = team[id];
 
   return (
-    <div className={`MemberPage MemberPage--${id}`}>
-      <SideMenuContainer />
-      <div className="MemberPage__wrapper">
-        <h1 className="MemberPage__title">{`${member.name} | ID #${id}`}</h1>
-        <h2 className="MemberPage__subtitle">{member.instrument}</h2>
-        <article>
-          {member.descriptions.map((description, i) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <p key={i} className="MemberPage__description">
-              {description}
-            </p>
-          ))}
-        </article>
-      </div>
-      <div className="MemberPage__copyright-wrapper">
-        <Copyright />
+    <div className="MemberPage__outer">
+      <MemberPageInput />
+      <div className={`MemberPage MemberPage--${id}`}>
+        <SideMenuContainer />
+        <div className="MemberPage__wrapper">
+          <h1 className="MemberPage__title">{`${member.name} | ID #${id}`}</h1>
+          <h2 className="MemberPage__subtitle">{member.instrument}</h2>
+          <article>
+            {member.descriptions.map((description, i) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <p key={i} className="MemberPage__description">
+                {description}
+              </p>
+            ))}
+          </article>
+          <label className="MemberPage__label" htmlFor="themeChange">Rock mode</label>
+        </div>
+        <div className="MemberPage__copyright-wrapper">
+          <Copyright />
+        </div>
       </div>
     </div>
   );
