b021cf0b4e5446f3d1299a51e88921e98d423d7d
Maksym Koval1 <Maksym_Koval1@epam.com>
2020-03-12 18:36:22 +0200

    Little fix, added destructuring

--- src/components/CustomMember/CustomMember_0/CustomMember_0.js
+++ src/components/CustomMember/CustomMember_0/CustomMember_0.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './CustomMember_0.scss';
 
-export default function CustomMember_0({ state, toggle }) {
-  const rockModeState = state =>
+export default function CustomMember_0(props) {
+  const { state, toggle } = props;
+  const getClassName = state =>
     state ? 'CustomMember0__switcher mode-on' : 'CustomMember0__switcher';
 
   return (
     <div className="CustomMember0">
       <div className="CustomMember0__controller">
         <div className="CustomMember0__text">Light theme</div>
-        <div className={rockModeState(state)} onClick={toggle}></div>
+        <div className={getClassName(state)} onClick={toggle}></div>
       </div>
-      <img  className="CustomMember0__image" />
     </div>
   );
 }
