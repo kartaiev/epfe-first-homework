5ce1e839601fe1c6a218bd1f8dab205368c034a6
Maksym Koval1 <Maksym_Koval1@epam.com>
2020-03-12 09:51:40 +0200

    adapted changes before merging

--- src/pages/Member/Member.js
+++ src/pages/Member/Member.js
@@ -19,7 +19,6 @@ const Member = props => {
   const member = team[id];
 
   return (
-<<<<<<< HEAD
     <div className="MemberPage__outer">
       <input
         id="themeChange"
@@ -29,8 +28,15 @@ const Member = props => {
       <div className={`MemberPage MemberPage--${id}`}>
         <SideMenuContainer />
         <div className="MemberPage__wrapper">
-          <h1 className="MemberPage__title">{`${member.name} | ID #${id}`}</h1>
-          <h2 className="MemberPage__subtitle">{member.instrument}</h2>
+          <h1 className="MemberPage__name">{`${member.name} | ID #${id}`}</h1>
+          <h2 className="MemberPage__info">
+          <span className="MemberPage__instrument">{member.instrument}</span>
+          <span className="MemberPage__contacts Contact">
+            {member.contacts.map(contact => (
+              <Contact key={contact.link} info={contact} />
+            ))}
+          </span>
+          </h2>
           <article>
             {member.descriptions.map((description, i) => (
               // eslint-disable-next-line react/no-array-index-key
