9cad0c597fa87174b6841a6d8b2ffb1a3cd97d53
Volodymyr <kartaiev@gmail.com>
2020-03-12 18:10:56 +0200

    custom member page done

--- src/pages/Member/Member.js
+++ src/pages/Member/Member.js
@@ -8,6 +8,9 @@ import './Member.scss';
 
 import team from '../../data/team.json';
 import teamPics from '../../assets/images/membersPics';
+import Toggle from '../../utilits/toggle';
+import song from '../../assets/audio/Highway.mp3';
+import musicFunc from '../../utilits/musicFunc';
 
 const Member = props => {
   const {
@@ -20,34 +23,44 @@ const Member = props => {
   const memberPic = teamPics[id];
 
   return (
-    <div
-      className={`MemberPage MemberPage--${id}`}
-      style={{ backgroundImage: `url(${memberPic})` }}
-    >
-      <SideMenuContainer />
-      <div className="MemberPage__wrapper">
-        <h1 className="MemberPage__name">{member.name}</h1>
-        <h2 className="MemberPage__info">
-          <span className="MemberPage__instrument">{member.instrument}</span>
-          <span className="MemberPage__contacts Contact">
-            {member.contacts.map(contact => (
-              <Contact key={contact.link} info={contact} />
-            ))}
-          </span>
-        </h2>
-        <article>
-          {member.descriptions.map((description, i) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <p key={i} className="MemberPage__description">
-              {description}
-            </p>
-          ))}
-        </article>
-      </div>
-      <div className="MemberPage__copyright-wrapper">
-        <Copyright />
-      </div>
-    </div>
+    <Toggle>
+      {({ state, toggle }) => (
+        <>
+          <div
+            onClick={+id === 2 ? toggle : null}
+            className={`MemberPage MemberPage--${id}`}
+            style={{ backgroundImage: `url(${memberPic})` }}
+          >
+            {+id === 2 && musicFunc(song, toggle, state)}
+            <SideMenuContainer />
+            <div className="MemberPage__wrapper">
+              <h1 className="MemberPage__name">{member.name}</h1>
+              <h2 className="MemberPage__info">
+                <span className="MemberPage__instrument">
+                  {member.instrument}
+                </span>
+                <span className="MemberPage__contacts Contact">
+                  {member.contacts.map(contact => (
+                    <Contact key={contact.link} info={contact} />
+                  ))}
+                </span>
+              </h2>
+              <article>
+                {member.descriptions.map((description, i) => (
+                  // eslint-disable-next-line react/no-array-index-key
+                  <p key={i} className="MemberPage__description">
+                    {description}
+                  </p>
+                ))}
+              </article>
+            </div>
+            <div className="MemberPage__copyright-wrapper">
+              <Copyright />
+            </div>
+          </div>
+        </>
+      )}
+    </Toggle>
   );
 };
 
